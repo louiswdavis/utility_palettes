70aef87d63716f68233dbfe3bf2a4c53148eccea
Louis Davis <79977953+louiswdavis@users.noreply.github.com>
2025-06-16 20:47:52 +0100

    initial setup

---
+++ spec/utility_palettes_rails_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe UtilityPalettesRails do
+  it "has a version number" do
+    expect(UtilityPalettesRails::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
